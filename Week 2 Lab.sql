e90e004a3dbe2a2be315c6f9eb3c14c37a520b9a
Marty <martorchard@gmail.com>
2024-06-17 14:02:44 +1200

    Update Week 2 Lab.sql
    Indents
    
    Temporary columns and updating them
    
    Challenge from week 2 Lab

--- Week 2 Lab.sql
+++ Week 2 Lab.sql
@@ -1,11 +1,8 @@
 use [master]
-go
-
-USE [master]
-go
+	go
 
 drop table if exists inventory;
-go
+	go
 
 create table inventory (
 	id int identity(1,1) primary key,
@@ -17,7 +14,7 @@ create table inventory (
 	acquired_date date default getdate(),
 	instock bit,
 	created_date smalldatetime default getdate()
-);
+	);
 go
 
 ---- Correct usage of string data types
@@ -42,34 +39,36 @@ go
 -- Start of week 1 exericse
 
 insert into inventory (name, colour, unit_price, rec_retail_price, qoh, acquired_date, instock, created_date)
-VALUES 
-	('Green Chair', 'Green', 45.99, 55.99, 10, '2024-02-27', 1, '2024-02-27 14:00:00'),
-	('Blue Chair', 'Blue', 50.00, 60.00, 5, '2024-03-01',1, '2024-03-01 10:00:00');
+	VALUES 
+		('Green Chair', 'Green', 45.99, 55.99, 10, '2024-02-27', 1, '2024-02-27 14:00:00'),
+		('Blue Chair', 'Blue', 50.00, 60.00, 5, '2024-03-01',1, '2024-03-01 10:00:00');
+		go
 
 --insert into inventory
---Values (
---	'Blue Chair', 'Blue', 50.00, 60.00, 5, '2024-03-01',1, '2024-03-01 10:00:00' -- If entering into a table this way you must enter the values in the order in which the table is layed out e.g name first then colour and so on.
---);
+	--Values (
+	--	'Blue Chair', 'Blue', 50.00, 60.00, 5, '2024-03-01',1, '2024-03-01 10:00:00' -- If entering into a table this way you must enter the values in the order in which the table is layed out e.g name first then colour and so on.
+	--);
 
 insert into inventory (name, colour, qoh, instock)
-values 
-('Red Chair', 'Red', 15, 1),
-('Red Table', 'red', 15, 1),
-('Blue Table', 'Blue', 15, 1),
-('Green Table', 'Green', 15, 1), 
-('Yellow Sofa', 'Yellow', 5, 1),
-('Purple Lamp', 'Purple', 7, 0),
-('Orange Desk', 'Orange', 3, 1),
-('Whiteboard', 'White', 10, 1),
-('Black Chair', 'Black', 15, 0),
-('Grey Table', 'Grey', 8, 1);
+	values 
+	('Red Chair', 'Red', 15, 1),
+	('Red Table', 'red', 15, 1),
+	('Blue Table', 'Blue', 15, 1),
+	('Green Table', 'Green', 15, 1), 
+	('Yellow Sofa', 'Yellow', 5, 1),
+	('Purple Lamp', 'Purple', 7, 0),
+	('Orange Desk', 'Orange', 3, 1),
+	('Whiteboard', 'White', 10, 1),
+	('Black Chair', 'Black', 15, 0),
+	('Grey Table', 'Grey', 8, 1);
+	go
 
 --Altering records:
 
 -- Update a specific record
 update inventory set
 	qoh = 8
-where id =1;
+	where id =1;
 
 -- Update multiple records
 update inventory set 
@@ -81,8 +80,8 @@ update inventory set
 
 -- Adding New column(s)
 alter table inventory
-add category varchar(50);
-go
+	add category varchar(50);
+	go
 -- Populate new column
 update inventory set category = case
 	when name LIKE '%Chair%' then 'chairs'
@@ -93,29 +92,49 @@ update inventory set category = case
 	else 'misc'
 	end;
 
+-- Temporary column
+alter table inventory
+	add tmp_name varchar(50), tmp_colour varchar(50);
+	go
+
+update inventory set 
+	tmp_name = name,
+	tmp_colour = colour;
+	go
+
+-- Challenge time!
+alter table inventory
+	add inv_name varchar(50), colours varchar(50);
+	go
+
+update inventory set
+	inv_name = name,
+	colours = colour;
+	go
+
+select * from inventory
+	
 
 
 
 
+-- Bunch of select queries for week 1
+	--select * from inventory where unit_price > 20.00 -- Pulls records where unit price is greater than 20
 
+	--select * from inventory where qoh != 0 -- Pulls all records where qoh is not equal to 0
 
+	--select * from inventory where rec_retail_price < 100 -- Pulls all records where rec retial price is less than 100
 
---select * from inventory where unit_price > 20.00 -- Pulls records where unit price is greater than 20
+	--select * from inventory where acquired_date < '2024-03-01' -- Pulls all records where the acquired date was before 2024-03-01
 
---select * from inventory where qoh != 0 -- Pulls all records where qoh is not equal to 0
+	--select * from inventory where name like '%Table%' -- Pulls all records that have the word 'table' in them
 
---select * from inventory where rec_retail_price < 100 -- Pulls all records where rec retial price is less than 100
+	--select * from inventory where colour in ('Red','Blue','Green') -- Pulls all records that have the colours stated 
 
---select * from inventory where acquired_date < '2024-03-01' -- Pulls all records where the acquired date was before 2024-03-01
+	--select * from inventory where unit_price > 30.00 and instock = 1 -- Pulls all records that have a unit price greater than 30 and is instock 
 
---select * from inventory where name like '%Table%' -- Pulls all records that have the word 'table' in them
+	--select * from inventory where instock = 0 or unit_price < 30.00 -- Pulls all records that are either not in stock OR have a unit price less than 30 
 
---select * from inventory where colour in ('Red','Blue','Green') -- Pulls all records that have the colours stated 
+	--select * from inventory where not instock =1; -- Pulls are records that are not instock   
 
---select * from inventory where unit_price > 30.00 and instock = 1 -- Pulls all records that have a unit price greater than 30 and is instock 
-
---select * from inventory where instock = 0 or unit_price < 30.00 -- Pulls all records that are either not in stock OR have a unit price less than 30 
-
---select * from inventory where not instock =1; -- Pulls are records that are not instock   
-
---select * from inventory -- Pulls all records in the inventory table
+	--select * from inventory -- Pulls all records in the inventory table
