10cf35ec650c7836da2ae2e045466f7593c9abe9
Marty <martorchard@gmail.com>
2024-06-17 15:09:53 +1200

    Update Week 4 Lab.sql
    Create actors + films

--- Week 4 Lab.sql
+++ Week 4 Lab.sql
@@ -5,3 +5,17 @@ create table directors (
 	 id int primary key,
 	 director varchar(255) not null
 );
+
+create table actors (
+	id int primary key,
+	actor_name varchar(255) not null
+);
+
+create table films (
+	id int primary key,
+	film_name varchar(255) not null
+	director_id int,
+	foreign key (director_id) references directors(id)
+
+
+);
